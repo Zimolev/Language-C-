5dae03049c1455422c423f903ca5748e57eb3421
zimolev <zimolev@yandex.ru>
2022-12-15 20:10:15 +0400

    Added 7th Example

---
+++ Example_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ввидите имя пользователя: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(userName);
+}
